e513b3aa8f804f4c4f0fa2484f8e8ee21eb6312f
Steve Brown <brown2020@gmail.com>
2024-09-18 16:07:20 -0700

    bugs

--- src/app/live/[room]/page.tsx
+++ src/app/live/[room]/page.tsx
@@ -2,22 +2,20 @@
 "use client";
 
 import React from "react";
-import { useRouter } from "next/navigation";
 import JoinForm from "@/components/JoinForm";
 import { selectIsConnectedToRoom, useHMSStore } from "@100mslive/react-sdk";
+import Livestream from "@/components/Livestream";
 
 export default function RoomPage({ params }: { params: { room: string } }) {
-  const router = useRouter();
   const isConnected = useHMSStore(selectIsConnectedToRoom);
-
-  if (isConnected) {
-    router.push("/live");
-    return null;
-  }
 
   // Extract the room parameter from the dynamic route segment
   const { room } = params;
 
   // Pass both role and initialRoom to JoinForm
-  return <JoinForm role="guest" initialRoom={room || ""} />;
+  return isConnected ? (
+    <Livestream />
+  ) : (
+    <JoinForm role="guest" initialRoom={room} />
+  );
 }
