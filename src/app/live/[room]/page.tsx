f26d8666c1ec7963eaa4d55ea3b67e35a408b115
Steve Brown <brown2020@gmail.com>
2024-09-18 16:09:30 -0700

    bugs

--- src/app/live/[room]/page.tsx
+++ src/app/live/[room]/page.tsx
@@ -1,16 +1,41 @@
 // app/[room]/page.tsx
 "use client";
 
-import React from "react";
+import React, { useCallback, useEffect } from "react";
 import JoinForm from "@/components/JoinForm";
-import { selectIsConnectedToRoom, useHMSStore } from "@100mslive/react-sdk";
+import {
+  selectIsConnectedToRoom,
+  useHMSActions,
+  useHMSStore,
+} from "@100mslive/react-sdk";
 import Livestream from "@/components/Livestream";
 
 export default function RoomPage({ params }: { params: { room: string } }) {
   const isConnected = useHMSStore(selectIsConnectedToRoom);
+  const hmsActions = useHMSActions();
 
   // Extract the room parameter from the dynamic route segment
   const { room } = params;
+
+  // Memoized cleanup function
+  const handleUnload = useCallback(() => {
+    if (isConnected) {
+      hmsActions.leave();
+    }
+  }, [hmsActions, isConnected]);
+
+  // Set log level and handle leaving the room on component unmount
+  useEffect(() => {
+    hmsActions.setLogLevel(4);
+
+    // Add event listener for window unload
+    window.addEventListener("beforeunload", handleUnload);
+
+    return () => {
+      window.removeEventListener("beforeunload", handleUnload);
+      handleUnload(); // Ensure room is left on component unmount
+    };
+  }, [hmsActions, handleUnload]);
 
   // Pass both role and initialRoom to JoinForm
   return isConnected ? (
