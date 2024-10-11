c93ee3b04ff0ef9cd2c1125c377fd92a938ace94
developeranku <ankur4736@gmail.com>
2024-10-11 18:04:19 +0530

    init shadcn and add button component

---
+++ src/components/Forms/AuthForm.tsx
@@ -0,0 +1,12 @@
+import { GoogleAuthButton } from "@chill-ui";
+
+const AuthForm = () => {
+  return (
+    <div>
+      <h1>This is Login Page</h1>
+      <GoogleAuthButton />
+    </div>
+  );
+};
+
+export default AuthForm;
