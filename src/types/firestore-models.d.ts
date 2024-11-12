d8c26b6ae91cd089e5db2ac56548315e37de0190
developeranku <ankur4736@gmail.com>
2024-11-12 15:07:10 +0530

    leave/end session functionality

---
+++ src/types/firestore-models.d.ts
@@ -0,0 +1,25 @@
+type MeetingSession = {
+  app_id: string;
+  broadcaster: string;
+  created_at: Date;
+  customer: string;
+  customer_id: string;
+  description: string;
+  enabled: boolean;
+  id: string;
+  large_room: boolean;
+  name: string;
+  recording_info: {
+    [key: string]: unknown;
+  };
+  region: string;
+  template: string;
+  template_id: string;
+  updated_at: Date;
+  is_recording_ready?: boolean; // Optional as it is present in only one instance
+  recording_storage_path?: string; // Optional for the same reason
+  room_id?: string; // Optional as it is present in only one instance
+  session_duration?: number; // Optional as it is present in only one instance
+};
+
+type MeetingSessions = MeetingSession[];
