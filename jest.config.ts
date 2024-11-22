083735377b41884cb784f896d547a42fe16aaeb2
developeranku <ankur4736@gmail.com>
2024-11-22 15:08:24 +0530

    put unit tests for auth form

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,7 @@
 import type { Config } from "jest";
+import { pathsToModuleNameMapper } from "ts-jest";
 import nextJest from "next/jest.js";
+import tsConfig from "./tsconfig.json";
 
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
@@ -13,6 +15,9 @@ const config: Config = {
   coverageProvider: "v8",
   testEnvironment: "jsdom",
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  moduleNameMapper: pathsToModuleNameMapper(tsConfig.compilerOptions.paths, {
+    prefix: "<rootDir>/",
+  }),
 };
 
 export default createJestConfig(config);
