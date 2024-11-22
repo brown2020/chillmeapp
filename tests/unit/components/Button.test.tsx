9710c3e7dcb9116ccf09689dee1ff4d6fabee105
developeranku <ankur4736@gmail.com>
2024-11-22 13:08:05 +0530

    update config

---
+++ tests/unit/components/Button.test.tsx
@@ -0,0 +1,9 @@
+import { render, screen } from "@testing-library/react";
+import { Button } from "../../../src/frontend/components/ui/Button";
+
+describe("Button component", () => {
+  it("should render", async () => {
+    render(<Button variant={"secondary"}>Click Me</Button>);
+    expect(screen.getByText("Click Me")).toBeInTheDocument();
+  });
+});
