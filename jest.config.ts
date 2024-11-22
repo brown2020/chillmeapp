9a6cec3c8e66b1313ea8c0e108a0f710b7d39616
developeranku <ankur4736@gmail.com>
2024-11-22 12:41:49 +0530

    add jest setup config files

---
+++ jest.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from "jest";
+import nextJest from "next/jest.js";
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: "./",
+});
+
+const config: Config = {
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  testEnvironment: "jsdom",
+  roots: ["<rootDir>/tests/"],
+};
+
+export default createJestConfig(config);
---
+++ tests/integration/example.test.ts
@@ -0,0 +1,3 @@
+test("Check if 1+1 is 2", () => {
+  expect(1 + 1).toBe(2);
+});
