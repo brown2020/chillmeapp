ab9e9607219d7a9a6cd8729ffd5d7decbbd7ca35
developeranku <ankur4736@gmail.com>
2024-11-22 14:15:05 +0530

    fix module mapping issue

--- tests/unit/components/Button.test.tsx
+++ tests/unit/components/Button.test.tsx
@@ -1,6 +1,5 @@
-import React from "react";
 import { render, screen } from "@testing-library/react";
-import { Button } from "../../../src/frontend/components/ui/Button";
+import { Button } from "@chill-ui";
 
 describe("Button component", () => {
   it("should render", async () => {
